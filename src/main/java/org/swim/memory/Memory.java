c924846f219a6a102d6c17770e443528d159da2c
swim5678 <154345558+Swim5678@users.noreply.github.com>
2026-03-02 13:23:30 +0800

    Initial commit

---
+++ src/main/java/org/swim/memory/Memory.java
@@ -0,0 +1,17 @@
+package org.swim.memory;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Memory extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
